254441c3d84ee8f8f2c4a63bd360b39b79aa1347
Walway <evgesnabk@gmail.com>
2026-09-15 02:05:05 +0530

    datamine: update Roblox assets (2026-09-14 20:35:05 UTC)
    Added: 1
    Updated: 0
    Removed: 0
    
    Public dump only — tracker source not included.

--- js.rbxcdn.com/DynamicLocalizationResourceScript_Feature.NotApproved.js
+++ js.rbxcdn.com/DynamicLocalizationResourceScript_Feature.NotApproved.js
@@ -516,7 +516,8 @@ Roblox.LangDynamic["Feature.NotApproved"] = {
   "Description.WarningDescription.Kids": "You broke a rule. You may get suspended if this goes on.",
   "Description.BannedDescription.Kids": "You can’t use this account any more.",
   "Action.Pending": "Pending",
-  "Action.ViewAppealGuidelines": "View appeal guidelines"
+  "Action.ViewAppealGuidelines": "View appeal guidelines",
+  "Label.AbuseType.ChildEndangerment.V4": "Child Safety"
 };
 window.Roblox &&
   window.Roblox.BundleDetector &&
